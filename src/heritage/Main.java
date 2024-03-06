8479b505ecd6f0a9841db8f077a58ea974bb1f3f
cheikh gueye <cheikhgueye191@gmail.com>
2024-03-06 21:32:13 +0000

    projet init

---
+++ src/heritage/Main.java
@@ -0,0 +1,18 @@
+package heritage;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		 
+		 for(int i=0 ; i<=1000; i++) {;
+			 boucle("heritage: "+Integer.toString(i));
+
+		 } 
+
+		}
+
+		 static void boucle(String nom) {
+		      System.out.println("Bonjour " + nom + "  !!!");
+		   }
+
+}
